8673f311477425ec376bbf24bb6d4adf868c5f45
resty <restykyomuhang@gmail.com>
2024-07-08 14:09:27 +0600

    changes

--- mochatest/test/appTest.js
+++ mochatest/test/appTest.js
@@ -10,10 +10,19 @@ describe ('App', function(){
     })
 
     it('should return 0 when multiplying with 0', function() {
-        assert.equal(multiply(5, 0), 0);
+        assert.equal(multiply(2, 2), 4);
+    });
+    it('should return 0 when multiplying with 0', function() {
+        assert.equal(multiply(3, 3), 9);
+    });
+    it('should return 0 when multiplying with 0', function() {
+        assert.equal(multiply(4, 4), 16);
+    });
+    it('should return 0 when multiplying with 0', function() {
+        assert.equal(multiply(23, 45), 23*45);
     });
 
-    // Add more test cases as needed
+    // For adding more test cases 
 })
 
 
