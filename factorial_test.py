07d9aceb7f29cef7f571ba7c6f00e13c0df5a702
resty <restykyomuhang@gmail.com>
2024-07-09 14:45:03 +0600

    Tests

--- factorial_test.py
+++ factorial_test.py
@@ -2,5 +2,8 @@ import factorial
 
 class Testfactorial:
     def test_factorial(self):
+        assert factorial.factorial(1) == 1
+        assert factorial.factorial(2) == 2
         assert factorial.factorial(3) == 6
+        assert factorial.factorial(4) == 24
 
