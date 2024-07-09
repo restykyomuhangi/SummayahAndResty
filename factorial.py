5b8fff5012e7177af9189b8651914cffe84f4523
resty <restykyomuhang@gmail.com>
2024-07-09 14:28:18 +0600

    failed test

---
+++ factorial.py
@@ -0,0 +1,5 @@
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return 1
---
+++ factorial_test.py
@@ -0,0 +1,6 @@
+import factorial
+
+class Testfactorial:
+    def test_factorial(self):
+        assert factorial.factorial(3) == 6
+
