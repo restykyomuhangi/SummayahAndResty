4a8d2f2aca1e004b554f5e87e7164627e5055666
resty <restykyomuhang@gmail.com>
2024-07-10 13:53:39 +0600

    added another test for n == 1

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -3,3 +3,5 @@ import fibonacci
 class Testfibonacci:
     def test_fibonacci(self):
         assert fibonacci.fibonacci(0) == 0
+    def test_fibonacci1(self):
+        assert fibonacci.fibonacci(1) == 1
